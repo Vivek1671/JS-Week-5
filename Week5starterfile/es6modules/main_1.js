f24d9ad308abb145f361b9051c79756ded93a876
Vivek <111949010+Vivek1671@users.noreply.github.com>
2022-12-10 16:26:01 +0530

    Add files via upload

---
+++ Week5starterfile/es6modules/main_1.js
@@ -0,0 +1,4 @@
+import { hello,funcs } from "./module_1.js"
+document.getElementById("app").innerHTML = hello;
+funcs.foo()
+funcs.bar();
---
+++ Week5starterfile/es6modules/module_1.js
@@ -0,0 +1,12 @@
+const funcs = {
+    foo: function()
+    {
+        alert("foo");
+    },
+    bar: function()
+    {
+        alert("bar");
+    },
+};
+const hello = "<h1>Hello</h1>"
+export {hello,funcs};
